b87eba76c0232138fa7ea965a4dda0bc271931e9
Vipul <tailorvipul@gmail.com>
2024-04-17 20:31:46 +0800

    Final Commit

--- src/main/java/net/vipul/Runner.java
+++ src/main/java/net/vipul/Runner.java
@@ -1,15 +1,12 @@
 package net.vipul;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Runner implements CommandLineRunner {
 
-    @Value("${app.version}")
-    private String appVersion;
     /**
      * Pull in the JFrame to be displayed.
      */
--- src/main/java/net/vipul/SpringBootSwingLmaxApplication.java
+++ src/main/java/net/vipul/SpringBootSwingLmaxApplication.java
@@ -1,6 +1,5 @@
 package net.vipul;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,9 +7,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootSwingLmaxApplication {
-    @Value("${app.version}")
-    private String appVersion;
-    
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(SpringBootSwingLmaxApplication.class)
                 .headless(false)
